4848d85d91ecd170c8fc2b1b4273d9dc13f77f42
ainoal <77739788+ainoal@users.noreply.github.com>
2023-12-06 20:57:22 +0200

    State after 6.12. session

--- preprocessing.m
+++ preprocessing.m
@@ -9,23 +9,34 @@ function [data_out] = preprocessing(data,skew_mode)
     else
         
         data_out = data;
+        %data_out(:,1:1)=smoothdata(data_out(:,1:1),'gaussian');
+        %data_out(:,2:2)=smoothdata(data_out(:,2:2),'gaussian');
+        %data_out(:,3:3)=smoothdata(data_out(:,3:3),'gaussian');
         % Window size of 5 and 7 deliver the best results
-        %{
-        window = 5;
-        b =(1/window)*ones(1,window);
-        a=1;
-        data_out(:,1:1)=filter(b,a,data_out(:,1:1));
-        data_out(:,2:2)=filter(b,a,data_out(:,2:2));
-        data_out(:,3:3)=filter(b,a,data_out(:,3:3));
+        
+        %window = 5;
+        %b =(1/window)*ones(1,window);
+        %a=1;
+        %data_out(:,1:1)=filter(b,a,data_out(:,1:1));
+        %data_out(:,2:2)=filter(b,a,data_out(:,2:2));
+        %data_out(:,3:3)=filter(b,a,data_out(:,3:3));
         
         data2d=data_out(:,1:2);
+        
         % Best results achieved by using moving mean for each coordinate
         % set and then appliying lowpass filter over second dimension across coordinates
+        % Apply lowpass
         b2 = 1;
-        a2 = [1 -0.8];
-        data2d = filter(b2,a2,data2d,[],2);
-        data_out(:,1:2) =data2d;
-        %}
+        a2 = [1 -0.7];
+        data2d = filter(b2,a2,data2d,[],1);
+        b1 = [1 0.9];
+        a1 = 1;
+        data2d = filter(b1,a1,data2d,[],1);
+
+
+
+        data_out(:,1:2) = data2d;
+
         data_out = normalize(data_out,1);
     end
 end
